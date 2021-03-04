bedea0b76bb169b99747d6693ab2103494b52625
Egor Belousov <egor0288354@yandex.ru>
2021-03-04 21:12:55 +0300

    Task #3 (Image of Mars)

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, url_for
 
 app = Flask(__name__)
 
@@ -25,6 +25,23 @@ def promotion():
             <p>Мы сделаем обитаемыми безжизненные пока планеты.</p>
             <p>И начнем с Марса!</p>
             <p>Присоединяйся!</p>
+        </body>
+    </html>
+    '''
+
+
+@app.route('/image_mars')
+def image_mars():
+    return f'''
+    <!DOCTYPE html> 
+    <html>
+        <head>
+            <title>Привет, Марс!</title>
+        </head>
+        <body>
+            <h1>Жди нас, Марс!</h1>
+            <img src="{url_for('static', filename='img/mars.png')}" alt="Mars">
+            <p>Вот она какая, красная планета.</p>
         </body>
     </html>
     '''
