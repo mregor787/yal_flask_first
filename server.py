cc45e7c8db75ea3da83018cc204c3e4983d8d808
Egor Belousov <egor0288354@yandex.ru>
2021-03-04 21:59:56 +0300

    Task #4 (Ad with an image)

--- server.py
+++ server.py
@@ -47,5 +47,32 @@ def image_mars():
     '''
 
 
+@app.route('/promotion_image')
+def promotion_image():
+    return f'''
+    <!DOCTYPE html> 
+    <html>
+        <head>
+            <meta charset="utf-8">
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" 
+            rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" 
+            crossorigin="anonymous">
+            <link href="{url_for('static', filename='css/style.css')}"
+            rel="stylesheet">
+            <title>Колонизация</title>
+        </head>
+        <body>
+            <h1>Жди нас, Марс!</h1>
+            <img src="{url_for('static', filename='img/mars.png')}" alt="Mars">
+            <div class="alert alert-dark">Человечество вырастает из детства.</div>
+            <div class="alert alert-success">Человечеству мала одна планета.</div>
+            <div class="alert alert-secondary">Мы сделаем обитаемыми безжизненные пока планеты.</div>
+            <div class="alert alert-warning">И начнем с Марса!</div>
+            <div class="alert alert-danger">Присоединяйся!</div>
+        </body>
+    </html>
+    '''
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
